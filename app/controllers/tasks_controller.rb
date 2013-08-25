54ed8c7b074f10098be61c97e6d53f6614a28726
Daniel Stepp <willstepp@gmail.com>
2013-08-25 14:09:55 -0400

    added deployment script

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,7 @@ class TasksController < ApplicationController
   end
 
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
+  #verify :method => :post, :only => [ :destroy, :create, :update ],:redirect_to => { :action => :list }
 
   def list
     @tasks_pages, @tasks = paginate :tasks, :per_page => 10
