b6eaa4a990c748981a1d24de2726d115dfe1c67a
Daniel Stepp <willstepp@gmail.com>
2013-12-02 16:10:59 -0500

    added fix for google places autocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,17 @@
 //= require foundation
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){ 
+  $(document).foundation();
+
+  //disable fastclick on Google Places functionality, because it breaks touch input
+  $(document).on({
+    'DOMNodeInserted': function() {
+      $('.pac-item, .pac-item span', this).addClass('needsclick');
+    }
+  }, '.pac-container');
+
+});
 
 $('.noEnterFormSubmit').keypress(function(e){
     if ( e.which == 13 ) e.preventDefault();
